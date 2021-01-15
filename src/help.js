522316481699518634abf36bf091770539dd297d
ToinNetuh <gpantonioneto@gmail.com>
2021-01-15 11:45:33 -0300

    Commiter

---
+++ src/help.js
@@ -0,0 +1,97 @@
+const help = (prefix, pushname, botname, oname) => {
+	return `
+「 *Toin Bot* 」
+Oiin
+
+◪ *INFORMAÇÕES*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Criador: Toin
+  ❏ Numero do meu criador: https://wa.me/5562996638900
+  ❏ Grupo do Bot: https://chat.whatsapp.com/CLRNnixaxwM6l73bj3fPke
+
+  Outros Menus
+  ${prefix}nsfwmenu
+  ${prefix}menuadmin
+  ${prefix}toinmenu
+
+◪ *CRIAR*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}toimg
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}marvellogo
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}ninjalogo
+  ├─ ❏ ${prefix}water
+  ├─ ❏ ${prefix}firetext
+  └─ ❏ ${prefix}quotemaker
+◪ *PESQUISA*
+  │
+  ├─ ❏ ${prefix}wiki
+  ├─ ❏ ${prefix}wikien
+  ├─ ❏ ${prefix}nulis
+  └─ ❏ ${prefix}artinama
+◪ *DOWNLOADER*
+  │
+  ├─ ❏ ${prefix}pinterest
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  ├─ ❏ ${prefix}joox
+  ├─ ❏ ${prefix}joox2
+  ├─ ❏ ${prefix}trendtwit
+  └─ ❏ ${prefix}ytsearch
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo
+◪ *SOUND*
+  │
+  ├─ ❏ ${prefix}play
+  └─ ❏ ${prefix}tts
+◪ *MUSIC*
+  │
+  ├─ ❏ ${prefix}lirik
+  └─ ❏ ${prefix}chord
+◪ *ISLAM*
+  │
+  └─ ❏ ${prefix}quran
+◪ *STALK*
+  │
+  ├─ ❏ ${prefix}tiktokstalk
+  └─ ❏ ${prefix}igstalk
+◪ *WIBU*
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  └─ ❏ ${prefix}nekonime
+◪ *FUN*
+  │
+  ├─ ❏ ${prefix}alay
+  ├─ ❏ ${prefix}gantengcek
+  ├─ ❏ ${prefix}watak
+  ├─ ❏ ${prefix}hobby
+  ├─ ❏ ${prefix}game
+  └─ ❏ ${prefix}glitch
+◪ *INFORMATION*
+  │
+  ├─ ❏ ${prefix}bahasa
+  ├─ ❏ ${prefix}kodenegara
+  ├─ ❏ ${prefix}kbbi
+  ├─ ❏ ${prefix}fakta
+  ├─ ❏ ${prefix}infocuaca
+  └─ ❏ ${prefix}covid
+◪ *OTHER*
+  │
+  ├─ ❏ ${prefix}send
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex
+  ├─ ❏ ${prefix}exe
+  ├─ ❏ ${prefix}qrcode
+  ├─ ❏ ${prefix}quotes
+  └─ ❏ ${prefix}fml
+`
+}
+
+exports.help = help
